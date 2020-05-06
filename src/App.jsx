9c8f29c8ac91d5473331a3bac5072621fe2b3170
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-05-06 17:33:31 -0300

    Styled components and eslint added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
+import Main from './pages/Main';
+
 function App() {
   return (
-    <>
-      <p>App</p>
-    </>
+    <Main />
   );
 }
 
---
+++ src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      Main page
+    </div>
+  );
+};
+
+export default Main;
