2ca8e0ff8e24847a48a5ae75b73e43dc59cd40b1
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-05-07 17:15:53 -0300

    Handle base context state in Main component added

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useContext } from 'react';
+
+import GameplayContext from '../../context/gameplay';
+import { useKeyboardControls } from '../../hooks/useKeyboardControls';
+import { controls } from '../../enums/CarControls';
+
+import Car from '../../components/Car';
+import Modal from '../../components/Modal';
+import Paused from '../../components/Paused';
+
+import { Container, BackgroundContainer, GameContainer } from './styles';
+
+const Main = () => {
+  const action = useKeyboardControls();
+  const {
+    paused,
+    startGame,
+    handlePauseGame,
+    handleStartGame,
+  } = useContext(GameplayContext);
+
+  useEffect(() => {
+    if (action === controls.pause && startGame) {
+      handlePauseGame();
+    }
+  }, [action]);
+
+  const handleStartGameplay = () => {
+    handleStartGame();
+  };
+
+  return (
+    <Container>
+      <BackgroundContainer />
+      {!startGame && <Modal onStartGame={handleStartGameplay} />}
+      {!!paused && <Paused />}
+      <GameContainer>
+        {!!startGame && <Car />}
+      </GameContainer>
+    </Container>
+  );
+};
+
+export default Main;
