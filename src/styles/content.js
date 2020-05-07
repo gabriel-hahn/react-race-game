ee12aa90a8439b8463b5871326b10b85e9b1700a
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-05-07 17:42:22 -0300

    New obstacles images added

--- src/styles/content.js
+++ src/styles/content.js
@@ -1,4 +1,7 @@
 export const contentCdn = {
   roadBackground: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/49e38bac-5a6c-4505-b7c7-742c0514bb69/CENARIO_anima.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200506%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200506T234216Z&X-Amz-Expires=86400&X-Amz-Signature=90f7cbe93dc17ccd72d7a19eff4082033187916d29f7fb2e9b80c5b889f25dc3&X-Amz-SignedHeaders=host',
   car: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Ff05e46f1-8333-425c-a610-cc1e6cdda6d1%2FCARRO.png?table=block&id=3c21cc55-a270-4b7b-9f8a-0ce8bb9cbe1b&width=2330&cache=v2',
+  obstacle_2: 'https://w7.pngwing.com/pngs/93/245/png-transparent-brown-and-green-leaves-leaf-branch-animation-cartoon-cartoon-tree-branches-green-leaves-watercolor-leaves-grass-new.png',
+  obstacle_1: 'https://cdn.pixabay.com/photo/2014/12/22/00/03/rocks-576666_960_720.png',
+  obstacle_3: 'https://images.vexels.com/media/users/3/145827/isolated/preview/357f06ecbaaa77d750259c459c0ed55f-ilustra----o-de-rock-redondo-by-vexels.png',
 };
