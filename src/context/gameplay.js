a6fca770fe1a5f7d56a06365c8e66aa00dfedd8b
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-05-07 16:21:03 -0300

    Gameplay context created

---
+++ src/context/gameplay.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const GameplayContext = React.createContext();
+
+export const GameplayProvider = ({ children }) => {
+  const [paused, setPaused] = useState(false);
+  const [startGame, setStartGame] = useState(true);
+
+  const handlePauseGame = () => {
+    setPaused((previousPausedState) => !previousPausedState);
+  };
+
+  const handleStartGame = () => {
+    setStartGame((previousStartGameState) => !previousStartGameState);
+  };
+
+  return (
+    <GameplayContext.Provider value={{
+      handlePauseGame,
+      handleStartGame,
+      paused,
+      startGame,
+    }}
+    >
+      {children}
+    </GameplayContext.Provider>
+  );
+};
+
+export default GameplayContext;
